fc08cc2ddfde1a3bd0ec1ff718c1a6ff16cf2398
dwiky dias <diaslaksmana@gmail.com>
2020-08-09 15:06:06 +0700

    update loading

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //loading page
 function onReady(callback) {
-  var intervalID = window.setInterval(checkReady, 2500);
+  var intervalID = window.setInterval(checkReady, 3000);
   function checkReady() {
     if (document.getElementsByTagName('body')[0] !== undefined) {
       window.clearInterval(intervalID);

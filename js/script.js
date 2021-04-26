6962965749453fbaf5deadc79640ab710c1fdd15
dias laksmana <dias@softwareseni.com>
2021-04-26 15:59:53 +0700

    update

--- js/script.js
+++ js/script.js
@@ -16,6 +16,39 @@ onReady(function () {
   show('loading', false);
 });
 
+// Section
+var sections = $('section')
+, nav = $('nav')
+, nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+    bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+  , id = $el.attr('href');
+
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 700);
+
+  return false;
+});
+
 //progress bar
 window.onload = function(event) {
    if (i == 0) {
@@ -56,42 +89,44 @@ $(function() {
 });
 
 
-// Section
-var sections = $('section')
-, nav = $('nav')
-, nav_height = nav.outerHeight();
-
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
-
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-    bottom = top + $(this).outerHeight();
-
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
-      sections.removeClass('active');
-
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
-  });
-});
-
-nav.find('a').on('click', function () {
-  var $el = $(this)
-  , id = $el.attr('href');
-
-  $('html, body').animate({
-    scrollTop: $(id).offset().top - nav_height
-  }, 700);
-
-  return false;
-});
-
-
 //
 $('#toggle').click(function() {
  $(this).toggleClass('active');
  $('#overlay_menu').toggleClass('open');
 });
+
+
+//countdown
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  let birthday = "Sep 19, 2021 00:00:00",
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+              countdown = document.getElementById("countdown"),
+              content = document.getElementById("content");
+
+          headline.innerText = "Happy Wedding!";
+          countdown.style.display = "none";
+          content.style.display = "block";
+
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
